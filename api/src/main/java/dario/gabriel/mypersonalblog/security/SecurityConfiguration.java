aad5765c11b7a1c4cca333c413cfd41a1a39c927
GDDario <gd.dario.dev@gmail.com>
2023-09-25 19:51:08 -0300

    Cors liberation

--- api/src/main/java/dario/gabriel/mypersonalblog/security/SecurityConfiguration.java
+++ api/src/main/java/dario/gabriel/mypersonalblog/security/SecurityConfiguration.java
@@ -8,13 +8,17 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import lombok.RequiredArgsConstructor;
 
 @Configuration
 @EnableWebSecurity
+@EnableWebMvc
 @RequiredArgsConstructor
-public class SecurityConfiguration {
+public class SecurityConfiguration implements WebMvcConfigurer {
 	
 	private final JwtAuthenticationFilter jwtAuthenticationFilter;
 	private final AuthenticationProvider authenticationProvider;
@@ -26,5 +30,12 @@ public class SecurityConfiguration {
 				SessionCreationPolicy.STATELESS).and().authenticationProvider(authenticationProvider)
 		.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 		return http.build();
+	}
+	
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+        .allowedOrigins("http://localhost:4200")
+        .allowedHeaders("*")
+        .allowedMethods("GET", "PUT", "POST", "PATCH", "DELETE", "OPTIONS");
 	}
 }
