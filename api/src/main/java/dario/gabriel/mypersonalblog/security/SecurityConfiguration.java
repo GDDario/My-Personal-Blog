1aef62298a0e0e94c27911d8692c31f4b0226603
GDDario <gd.dario.dev@gmail.com>
2023-10-24 13:14:54 -0300

    fix: update security to allow default routes

--- api/src/main/java/dario/gabriel/mypersonalblog/security/SecurityConfiguration.java
+++ api/src/main/java/dario/gabriel/mypersonalblog/security/SecurityConfiguration.java
@@ -2,12 +2,17 @@ package dario.gabriel.mypersonalblog.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -25,17 +30,28 @@ public class SecurityConfiguration implements WebMvcConfigurer {
 
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-		http.csrf().disable().authorizeHttpRequests().requestMatchers("auth/**").permitAll()
+		http.csrf().disable().cors(Customizer.withDefaults()).authorizeHttpRequests()
+				.requestMatchers("auth/**").permitAll()
+				.requestMatchers(HttpMethod.GET, "post/**").permitAll()
+				.requestMatchers(HttpMethod.GET, "category/**").permitAll()
+				.requestMatchers(HttpMethod.GET, "comment/**").permitAll()
 		.anyRequest().authenticated().and().sessionManagement().sessionCreationPolicy(
 				SessionCreationPolicy.STATELESS).and().authenticationProvider(authenticationProvider)
 		.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 		return http.build();
 	}
 
-	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**")
-        .allowedOrigins("http://localhost:4200")
-        .allowedHeaders("*")
-        .allowedMethods("GET", "PUT", "POST", "PATCH", "DELETE", "OPTIONS");
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("http://localhost:4200") // Replace with your Angular app's origin
+						.allowedMethods("GET", "POST", "PUT", "DELETE")
+						.allowedHeaders("*")
+						.allowCredentials(true);
+			}
+		};
 	}
 }
