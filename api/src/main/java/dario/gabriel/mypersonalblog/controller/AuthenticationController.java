50c5ba21f14e1ed9fd7c45bec4ef3dafc604c866
GDDario <gd.dario.dev@gmail.com>
2023-10-24 13:12:55 -0300

    fix: optimize imports

--- api/src/main/java/dario/gabriel/mypersonalblog/controller/AuthenticationController.java
+++ api/src/main/java/dario/gabriel/mypersonalblog/controller/AuthenticationController.java
@@ -1,17 +1,16 @@
 package dario.gabriel.mypersonalblog.controller;
 
+import dario.gabriel.mypersonalblog.model.httpResponses.AuthenticationRequest;
+import dario.gabriel.mypersonalblog.model.httpResponses.AuthenticationResponse;
+import dario.gabriel.mypersonalblog.model.httpResponses.RegisterRequest;
+import dario.gabriel.mypersonalblog.service.AuthenticationService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import dario.gabriel.mypersonalblog.service.AuthenticationService;
-import dario.gabriel.mypersonalblog.model.httpResponses.AuthenticationRequest;
-import dario.gabriel.mypersonalblog.model.httpResponses.AuthenticationResponse;
-import dario.gabriel.mypersonalblog.model.httpResponses.RegisterRequest;
-import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/auth")
--- api/src/main/java/dario/gabriel/mypersonalblog/controller/CategoryController.java
+++ api/src/main/java/dario/gabriel/mypersonalblog/controller/CategoryController.java
@@ -8,11 +8,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import java.net.URI;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 
 @RestController
